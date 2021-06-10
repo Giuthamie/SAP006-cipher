6df23c68d4a0ef5d294db4bc4d6385e1dbacb344
giuthamie <giumie@hotmail.com>
2021-06-10 10:22:14 -0300

    Mudei textarea por inputs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
+let desloc;
+let cifra;
+let decifra;
+let resp;
+
+
+function clicar() {
+    let cifrar = document.getElementById("#cifrar")
+    a.innerText = ""
+}
 console.log(cipher);
