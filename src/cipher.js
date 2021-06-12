c62acde6325b045e8e27b3a2855eb12e66a8a128
giuthamie <giumie@hotmail.com>
2021-06-12 17:38:52 -0300

    Index.js com dom bugs a arrumar/cipher.js funcionando para maiusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
- const cipher = {
-  // ...
+const cipher = {
+
+encode: function (offset, string){
+  let textoCifrar = "";
+  let numberoffset = parseInt(offset);
+
+  for (let i= 0; i<=string.length; i++){
+
+  let codigomsgci = string.charCodeAt(i);
+  //Se for digitado maísculas de A a Z
+  if(codigomsgci >=65 && codigomsgci <=90){
+  let codigoformci = ((codigomsgci - 65 + numberoffset) %26) + 65;
+  textoCifrar+=String.fromCharCode(codigoformci);
+  }
+  }
+  console.log(textoCifrar);
+},
+  
+  
+decode: function(offset, string){
+  let textoDecifrar = "";
+  let numberoffset = parseInt(offset);
+
+  for (let i= 0; i<=string.length; i++){
+  let codigomsgde = string.charCodeAt(i);
+  
+  if(codigomsgde >=65 && codigomsgde <=90){
+  let codigoformde = 90-((90-codigomsgde + numberoffset) %26);
+  textoDecifrar+=String.fromCharCode(codigoformde);
+
+}
+}
+console.log(textoDecifrar);
+}
+
 };
+
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import cipher from './cipher.js';
 
-let desloc;
+// Criar as variáveis primeiro, declarando fora da function
+// o addEventListener vai fazer a ação funcionar
+// Pela ID chama para acontecer
+// .value para transformar o valor em número
+//  
+
+let offset;
 let cifra;
 let decifra;
 let resp;
 
+let botaocifrar = document.getElementById("button1")
+botaocifrar.addEventListener("click", cifrar);
 
-function clicar() {
-    let cifrar = document.getElementById("#cifrar")
-    a.innerText = ""
+let botaodecifrar = document.getElementById("button2")
+botaodecifrar.addEventListener("click", decifrar);
+
+function cifrar() {
+    offset = Number(document.getElementById("deslocamento").value)
+    cifra =  document.getElementById("cifrar").value
+    console.log(cifra)
+    resp =  document.getElementById("resposta")
+    return resp.value = cipher.encode(offset,cifra);
 }
+
+function decifrar () {
+    offset = Number(document.getElementById("deslocamento").value)
+    decifra = document.getElementById("decifrar").value
+    resp = document.getElementById("resposta")
+    return resp.value = cipher.encode(offset,decifra);
+}
+
+
 console.log(cipher);
